ad8a64c4bcdf994c119ec6edee1bb9891c87925e
mkreynolds919 <mkreynolds919@gmail.com>
2025-04-21 20:14:41 -0500

    Add testing for function calling

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 async function getData(location) {
     const request = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=SZV84J3X87FSVFXN8RAKDZR9H`, { mode : 'cors'});
     response =  await request.json();
-    console.log(response.latitude);
-
+    return response.latitude;
 }
 
-getData('london');
+
+let data = getData('london');
+data.then(function(response) {
+    console.log(response);
+})
 
 
