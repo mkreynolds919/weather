1cfd1832a3fd65d20002300705ab0db594e872e6
mkreynolds919 <mkreynolds919@gmail.com>
2025-04-25 18:02:52 -0500

    Create new object with data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 async function getData(location) {
     const request = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=SZV84J3X87FSVFXN8RAKDZR9H`, { mode : 'cors'});
     response =  await request.json();
-    return response.latitude;
+    data = await Location.createInstance(response);
+    return await data.temp;
 }
 
 
@@ -14,3 +15,24 @@ search.addEventListener("click", async () => {
     console.log(data);
 });
 
+class Location {
+    constructor() {
+        this.temp = null;
+        this.tempmax = null;
+        this.tempmin = null;
+        this.uvindex = null;
+        this.conditions = null;
+        this. precipttype = null;
+    }
+
+    static async createInstance(response) {
+        const instance = new Location();
+        instance.temp = await response.currentConditions.temp;
+        instance.tempmax = await response.days.tempmax;
+        instance.tempmin = await response.days.tempmin;
+        instance.uvindex = await response.uvindex;
+        instance.conditions = await response.conditions;
+        instance. precipttype = await response.preciptype;
+        return instance;
+    }
+}
