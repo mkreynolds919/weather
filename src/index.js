00ad2a8321ac0dd8eaad50b9b787c252f0444a64
mkreynolds919 <mkreynolds919@gmail.com>
2025-05-19 11:12:24 -0500

    Fix API JSON navigation to accurately get data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 async function getData(location) {
     const request = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=SZV84J3X87FSVFXN8RAKDZR9H`, { mode : 'cors'});
     response =  await request.json();
-    data = await Location.createInstance(response);
-    return await data.temp;
+    const data = await Location.createInstance(response);
+    return data;
 }
 
 
@@ -12,7 +12,7 @@ search.addEventListener("click", async () => {
     const locationInput = document.getElementById("location");
     let location = locationInput.value;
     const data = await getData(location);
-    console.log(data);
+    console.log(data.conditions);
 });
 
 class Location {
@@ -28,11 +28,11 @@ class Location {
     static async createInstance(response) {
         const instance = new Location();
         instance.temp = await response.currentConditions.temp;
-        instance.tempmax = await response.days.tempmax;
-        instance.tempmin = await response.days.tempmin;
-        instance.uvindex = await response.uvindex;
-        instance.conditions = await response.conditions;
-        instance. precipttype = await response.preciptype;
+        instance.tempmax = await response.days[0].tempmax;
+        instance.tempmin = await response.days[0].tempmin;
+        instance.uvindex = await response.currentConditions.uvindex;
+        instance.conditions = await response.currentConditions.conditions;
+        instance.preciptype = await response.currentConditions.preciptype;
         return instance;
     }
 }
